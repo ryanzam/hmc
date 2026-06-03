c9d7e9baf8bd140187b62aef707b6d1e973b6209
ᖇᗩᑎᒎᗩᗰ <ryanzam2005@yahoo.com>
2026-06-03 18:49:05 +0545

    make map responsive

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -77,7 +77,7 @@ export function Contact() {
             </div>
 
             <div className="w-full h-100" >
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.283098821798!2d85.27969713937978!3d27.70854417628097!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb196b361cd501%3A0x160f477d4822a7a1!2sHaven%20Management%20Consultants%20(P)%20Ltd.!5e0!3m2!1sen!2snp!4v1780490012403!5m2!1sen!2snp" width="100%" height="100%" style={{ border: 0 }} allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.283098821798!2d85.27969713937978!3d27.70854417628097!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb196b361cd501%3A0x160f477d4822a7a1!2sHaven%20Management%20Consultants%20(P)%20Ltd.!5e0!3m2!1sen!2snp!4v1780490012403!5m2!1sen!2snp" style={{ width: "100%", height: "100%", border: 0 }} allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
             </div>
 
             <div id="contact-form" className="bg-surface py-24 lg:py-32">

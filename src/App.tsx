d3122cd65ec24e6ab502c69a18968d7230401e80
ᖇᗩᑎᒎᗩᗰ <ryanzam2005@yahoo.com>
2026-06-04 18:33:32 +0545

    add helmet

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,28 @@ import { AboutUs } from './components/AboutUs'
 import { Services } from './components/Services'
 import { Contact } from './components/Contact'
 import { Footer } from './components/Footer'
+import { Helmet } from 'react-helmet-async';
 
 function App() {
 
   return (
-    <main className="min-h-screen bg-surface">
-      <Navbar />
-      <Hero />
-      <AboutUs />
-      <Services />
-      <Contact />
-      <Footer />
-    </main>
+    <>
+      <Helmet>
+        <title>Haven Management Consultants | Professional Consulting Services Nepal</title>
+        <meta name="description" content="Leading financial and audit consulting services in Sitapaila, Kathmandu, Nepal." />
+        <link rel="canonical" href="https://havenconsultants.com.np" />
+        <meta property="og:title" content="Haven Consultants" />
+        <meta property="og:image" content="https://havenconsultants.com.npog-image.jpg" />
+      </Helmet>
+      <main className="min-h-screen bg-surface">
+        <Navbar />
+        <Hero />
+        <AboutUs />
+        <Services />
+        <Contact />
+        <Footer />
+      </main>
+    </>
   )
 }
 
